53f69c6ba4398a5cfcfe7ace04fb35739d7a4d82
Muhammed Erziku <106862443+MErziku@users.noreply.github.com>
2025-08-16 06:32:05 +0300

    Add files via upload

---
+++ calculate_discount.py
@@ -0,0 +1,11 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discount_amount = price * (discount_percent / 100)
+        final_price = price - discount_amount
+        return final_price
+    else:
+        return price
+    
+    
+print(calculate_discount(100, 25))
+print(calculate_discount(100, 10))
